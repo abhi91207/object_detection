e80c2cdf3db31791cb7e27c51a3518982040bec7
Abhishek Baghel <baghelabhi17@gmail.com>
2025-06-25 03:39:13 +0530

    initial commit

---
+++ lib/utils/my_custom_text_style.dart
@@ -0,0 +1,114 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+// font 12
+TextStyle myTextStyle12({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 12,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 15
+TextStyle myTextStyle15({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 15,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 18
+TextStyle myTextStyle18({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 18,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 21
+TextStyle myTextStyle21({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 21,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 24
+TextStyle myTextStyle24({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 24,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 32
+TextStyle myTextStyle32({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 32,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 48
+TextStyle myTextStyle48({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 48,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
+
+// font 54
+TextStyle myTextStyle54({
+  FontWeight fontWeight = FontWeight.normal,
+  Color fontColors = Colors.black,
+  String fontFamily = "main",
+}) {
+  return TextStyle(
+    fontWeight: fontWeight,
+    fontSize: 54,
+    color: fontColors,
+    fontFamily: fontFamily,
+  );
+}
